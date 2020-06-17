5c28430b82edc7c3d579d7fcded503b2a488c17e
Joe <jrwpatterson@gmail.com>
2020-06-17 13:42:35 +1000

    testing

--- src/old-branch-notify.ts
+++ src/old-branch-notify.ts
@@ -24,7 +24,7 @@ export async function oldBranchNotify(
     const branches = (
       await actionContext.octokit.git.listMatchingRefs({
         ...repoInfo,
-        ref: 'TER116-confusion!'
+        ref: 'TER116'
       })
     ).data
     actionContext.debug(JSON.stringify(branches[0]))
